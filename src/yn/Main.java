ae4e4dc6c6b60d6f06d356056a53ca03b3d426d0
Yitzchak Novick <yitzchak.novick@touro.edu>
2026-02-02 19:58:45 -0500

    Added the welcome message to the program

--- src/yn/Main.java
+++ src/yn/Main.java
@@ -3,6 +3,7 @@ package yn;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("**** WELCOME ****");
 		String msg = MessageGenerator.getMessage();
 		System.out.println(msg);
 	}
