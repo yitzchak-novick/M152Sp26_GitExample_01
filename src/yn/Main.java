adfecbb096ae9e2a9bb1ae4242727a60c278348e
yitzchak-novick <yitzchak.novick@touro.edu>
2026-02-02 20:02:00 -0500

    Fixed welcome message

--- src/yn/Main.java
+++ src/yn/Main.java
@@ -3,7 +3,7 @@ package yn;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("**** WELCOME ****");
+		System.out.println("*** MESSAGE GREETER V 1.2 ALL RIGHTS RESERVED ***");
 		String msg = MessageGenerator.getMessage();
 		System.out.println(msg);
 	}
