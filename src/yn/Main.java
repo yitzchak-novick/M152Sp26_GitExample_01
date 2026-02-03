3fb5dd152c3ab188138af5889b9d927beadd677c
Yitzchak Novick <yitzchak.novick@touro.edu>
2026-02-02 19:34:08 -0500

    refactored to use msg variable properly and updated message

--- src/yn/Main.java
+++ src/yn/Main.java
@@ -3,8 +3,7 @@ package yn;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(MessageGenerator.getMessage());
-
+		String msg = MessageGenerator.getMessage();
+		System.out.println(msg);
 	}
-
 }
