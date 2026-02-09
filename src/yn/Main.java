55926dca0df3eaa0d65fcad673a98595e29ed620
Yitzchak Novick <yitzchak.novick@touro.edu>
2026-02-09 18:32:40 -0500

    added some useful documentation

--- src/yn/Main.java
+++ src/yn/Main.java
@@ -2,8 +2,9 @@ package yn;
 
 public class Main {
 
+	// Main method
 	public static void main(String[] args) {
-		System.out.println("*** MESSAGE GREETER V 1.2 ALL RIGHTS RESERVED ***");
+		System.out.println("*** MESSAGE GREETER V 1.3 ALL RIGHTS RESERVED ***");
 		String msg = MessageGenerator.getMessage();
 		System.out.println(msg);
 	}
