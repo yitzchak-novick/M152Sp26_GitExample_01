9f6d1159d8659848835f7483f4bd0c5fabaf7aa4
yitzchak-novick <yitzchak.novick@touro.edu>
2026-02-09 18:36:28 -0500

    Rename msg variable for clarity

--- src/yn/Main.java
+++ src/yn/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	// Main method
 	public static void main(String[] args) {
 		System.out.println("*** MESSAGE GREETER V 1.3 ALL RIGHTS RESERVED ***");
-		String msg = MessageGenerator.getMessage();
-		System.out.println(msg);
+		String message = MessageGenerator.getMessage();
+		System.out.println(message);
 	}
 }
